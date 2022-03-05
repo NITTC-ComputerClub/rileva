1d02448e0c00d08858f5e074008f214050b862ab
Fumiya Sakaguchi <49513840+Chaha1n@users.noreply.github.com>
2022-03-05 21:58:38 +0900

    feat:import cdk from other way

--- bin/rileva.ts
+++ bin/rileva.ts
@@ -1,6 +1,5 @@
 #!/usr/bin/env node
-import 'source-map-support/register';
-import * as cdk from 'aws-cdk-lib';
+import * as cdk from '@aws-cdk/core';
 import { RilevaStack } from '../lib/rileva-stack';
 
 const app = new cdk.App();
